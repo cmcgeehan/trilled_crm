ed554e7a0f30ccd81341cb0e541a03b7015b2396
Connor McGeehan <conorm15@gmail.com>
2025-02-12 15:09:42 -0600

    get it

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -13,6 +13,8 @@ if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClientComponentClient<Database>({
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   options: {
     realtime: {
       params: {
